73201daf67df97ccaae9fff7adea12758a9b0c23
Al Burns <al.burns.email@gmail.com>
2020-02-28 14:25:29 -0800

    added models.py

--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+class Contact(models.Model):
+    class Meta:
+        app_label = 'contacts'
+
+    name = models.CharField(max_length=32)
+
 # Create your models here.
